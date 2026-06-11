8c1023fdb6226bb922c0ff2e20761df3e41a2989
Affan Shahid <affan.shahid.94@gmail.com>
2026-06-12 00:28:43 +0500

    feat: hook up database

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use anyhow::Result;
+use anyhow::{Result, anyhow};
 use clap::Parser;
 use sql_mcp_server::{cli::Cli, tunnel};
+use sqlx::any::AnyPoolOptions;
 use tokio::signal;
 use tracing::{Level, debug, error, info};
 use tracing_subscriber::EnvFilter;
@@ -16,18 +17,43 @@ async fn main() -> Result<()> {
         .init();
 
     let args = Cli::parse();
-    debug!("{:#?}", args);
+    debug!("Args: {:#?}", args);
 
-    let (tunnel_handle, port) = tunnel::create(args.ssh.unwrap(), args.database_url).await?;
-    info!("{port}");
+    let (url, handle) = match args.ssh {
+        Some(ssh_opts) => {
+            let mut forwarding_url = args.database_url.clone();
+            let (tunnel_handle, port) = tunnel::create(ssh_opts, args.database_url).await?;
+            forwarding_url.set_host(Some("127.0.0.1"))?;
+            forwarding_url
+                .set_port(Some(port))
+                .map_err(|_| anyhow!("cannot be base"))?;
 
-    tokio::select! {
-        _ = signal::ctrl_c() => (),
-        res = tunnel_handle =>match res {
-            Ok(_) => info!("Tunnel closed"),
-            Err(e) => error!("Tunnel panicked: {e}"),
+            debug!("Tunnel listening on 127.0.0.1:{port}");
+            (forwarding_url, Some(tunnel_handle))
         }
+        None => (args.database_url, None),
     };
+
+    sqlx::any::install_default_drivers();
+
+    let pool = AnyPoolOptions::new()
+        .max_connections(5)
+        .connect(&url.to_string())
+        .await?;
+
+    let (res,): (i64,) = sqlx::query_as("Select 67").fetch_one(&pool).await?;
+    info!("Response: {res}");
+
+    match handle {
+        Some(handle) => tokio::select! {
+            _ = signal::ctrl_c() => (),
+            res = handle => match res {
+                Ok(_) => info!("Tunnel closed"),
+                Err(e) => error!("Tunnel panicked: {e}"),
+            }
+        },
+        None => signal::ctrl_c().await?,
+    }
 
     Ok(())
 }
